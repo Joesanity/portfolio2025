7ec3dbf92eff49001cfb4765e88fc90ff00075fb
Joesanity <103509985+Joesanity@users.noreply.github.com>
2025-11-04 15:25:18 +0000

    Hero Section Updates
    - Added Description
    - Styling Updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,18 +6,33 @@ import { TbFileCv } from "react-icons/tb";
 export default function Home() {
   return (
     <main>
-      <section className="w-4/5 mx-auto flex gap-4">
+      <section className="w-4/5 mx-auto flex gap-4 my-12 items-stretch">
         <div className="flex flex-col w-1/2 gap-6">
           <div className="border-black border rounded p-6">
-            <p className="text-md">Software Engineer</p>
-            <h1 className="text-5xl font-extrabold mb-6">Joe Dinsley</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum quas fugiat asperiores nostrum fuga architecto omnis magnam esse consequuntur quis, quam laboriosam qui! Asperiores similique corporis vel, aliquam tempora excepturi.</p>
+            <p className="text-md mb-3">Software Engineer</p>
+            <h1 className="text-6xl font-extrabold mb-9">
+              Hello I'm Joe Dinsley
+            </h1>
+            <p>
+              Results-driven software engineer with experience across front-end
+              and back-end development. Skilled in building high-performance
+              websites and applications with a strong focus on SEO,
+              accessibility, and optimisation.
+            </p>
+            <br />
+            <p>
+              Proven ability to lead projects from concept to delivery,
+              collaborating with teams and clients to deliver efficient,
+              scalable, and high-quality solutions.
+            </p>
           </div>
-          <div className="border-black border rounded p-6 flex gap-6"><FaGithub size={36} /><FaLinkedin size={36} /><TbFileCv size={36} /></div>
+          <div className="border-black border rounded p-6 flex gap-6">
+            <FaGithub size={36} />
+            <FaLinkedin size={36} />
+            <TbFileCv size={36} />
+          </div>
         </div>
-        <div>
-          Image/Illustration
-        </div>
+        <div className="border-black border rounded p-6 w-1/2">Image/Illustration</div>
       </section>
     </main>
   );
