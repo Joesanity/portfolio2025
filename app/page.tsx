bae3106becf5ea389bf510b20f29ca8e84b5190d
Joesanity <103509985+Joesanity@users.noreply.github.com>
2025-11-06 22:47:42 +0000

    Hero changes
    - Changed font styles
    - Changed some spacing

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,12 @@ import { TbFileCv } from "react-icons/tb";
 export default function Home() {
   return (
     <main>
-      <section className="w-4/5 mx-auto flex gap-4 my-12 items-stretch">
-        <div className="flex flex-col w-1/2 gap-6">
-          <div className="border-black border rounded p-6">
+      <section className="w-4/5 mx-auto flex gap-9 my-12 items-stretch">
+        <div className="flex flex-col w-1/2 gap-9">
+          <div className="border-black border-4 rounded-t-lg p-6">
             <p className="text-md mb-3">Software Engineer</p>
-            <h1 className="text-6xl font-extrabold mb-9">
-              Hello I'm Joe Dinsley
+            <h1 className="text-6xl font-bold mb-9">
+              Hello, <span className="font-light">I'm Joe Dinsley</span>
             </h1>
             <p>
               Results-driven software engineer with experience across front-end
@@ -27,14 +27,19 @@ export default function Home() {
               scalable, and high-quality solutions.
             </p>
           </div>
-          <div className="border-black border rounded p-6 flex gap-6">
+          <div className="border-black border-4 rounded-b-lg p-6 flex gap-9">
             <FaGithub size={36} />
             <FaLinkedin size={36} />
             <TbFileCv size={36} />
           </div>
         </div>
-        <div className="border-black border rounded w-1/2">
-          <Cloud />
+        <div className="flex flex-col w-1/2 gap-9">
+          <div className="border-black border-4 rounded-t-lg flex-7">
+            <Cloud />
+          </div>
+          <div className="border-black border-4 rounded-b-lg flex-1 flex items-center justify-center">
+            <h2 className="text-center text-4xl font-bold">Technologies</h2>
+          </div>
         </div>
       </section>
     </main>
